c97398130e9b5399c737d0d36e4bacea6502b17d
LipeDev <152323104+Lipe-0x0@users.noreply.github.com>
2025-05-10 22:56:09 -0300

    adicionei os icones e  tambem a classe para entrada do texto

--- botoes.py
+++ botoes.py
@@ -1,8 +1,8 @@
 from tkinter import *
 
 class Botao:
-    def __init__(self, parent, text, comp, alt, pos_x, pos_y):
+    def __init__(self, parent, text, comp, alt, pos_x, pos_y, image = None, posicao_text = None, funcao = None):
         
-        self.botao = Button(parent, text=text) # Criando botao
+        self.botao = Button(parent, text=text, image=image, compound=posicao_text, command=funcao) # Criando botao
 
         self.botao.place(relx= pos_x, rely= pos_y, relheight=alt, relwidth=comp) # Botando botao no frame
--- interface_inicial.py
+++ interface_inicial.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from janelas import Janela
 from botoes import Botao
+from entradas import Entrada
 
 class Interface:
     def __init__(self, titulo,  col_image, tamanho_pag = None):
@@ -10,19 +11,26 @@ class Interface:
         self.raiz.configure(background=col_image) # Inserindo cor/imagem de fundo
 
     def botao(self):
-        self.opcao = Botao(self.janela1, text="Opção", comp=0.10,alt=0.10, pos_x=0.10, pos_y= 0.5)
+        icon_opcao = PhotoImage("icones/opcao (1).png")
+        icon_info = PhotoImage("icones/em-formacao.png")
+
+        self.opcao = Botao(self.janela2.janela, text="OPÇÃO", comp=0.2,alt=0.50, pos_x=0.10, pos_y= 0.3,
+                            image=icon_opcao, posicao_text=LEFT) # Botão de Opção
+
+        self.info = Botao(self.janela2.janela, text="INFO", comp=0.2,alt=0.50, pos_x=0.35, pos_y= 0.3,
+                          image=icon_info, posicao_text=LEFT) # Botão de informação
 
     def frame(self):
-        self.janela1 = Janela(t.raiz, comp=0.90, alt=0.70, pos_x=0.05, pos_y=0.14, 
-                 cor_back="red", cor_borda="black", larg_borda=10)
+        self.janela1 = Janela(self.raiz, comp=0.90, alt=0.70, pos_x=0.05, pos_y=0.14, 
+                 cor_back="red", cor_borda="black", larg_borda=10) # Frame onde escreve o texto
 
-        self.janela2 = Janela(t.raiz, comp=1, alt=0.10, pos_x=0, pos_y=0, 
-                 cor_back="green", cor_borda="black", larg_borda=5)
-        
-        self.botao()
+        self.janela2 = Janela(self.raiz, comp=1, alt=0.10, pos_x=0, pos_y=0, 
+                 cor_back="green", cor_borda="black", larg_borda=5) # Frame onde mostra os botões
+    
     
     def iniciar(self):
         self.frame()
+        self.botao()
         self.raiz.mainloop()
 
 
